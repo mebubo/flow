e3de651dc0d36d5aa1dee4e43fdfafa44cb27ca7
Sergei Dolgov <dolgovs@gmail.com>
2019-06-05 08:42:35 +0200

    Snapshot

--- src/main/scala/flow/Flow.scala
+++ src/main/scala/flow/Flow.scala
@@ -1,24 +1,39 @@
 package flow
+
 import cats.Traverse
 import cats.implicits._
 import cats.Applicative
+import cats.Monad
 
 case class Path(p: String)
 
 case class Input(path: Path)
 case class StoreInput(path: Path)
 
-case class Step(inputs: List[Input])
-case class ReifiedStep(inputs: List[StoreInput])
+case class Step(inputs: List[Input], cmd: Input)
+case class ReifiedStep(inputs: List[StoreInput], cmd: StoreInput, out: StoreInput)
+
+case class ExitCode(code: Int)
+
+case class Hash(s: String)
 
 trait Store[F[_]] {
     def load(i: Input): F[StoreInput]
+    def hash(is: List[StoreInput]): F[Hash]
+    def outputPathFromHash(h: Hash): F[StoreInput]
 }
 
-class Workflow[F[_] : Applicative](store: Store[F]) {
+class Workflow[F[_] : Monad](store: Store[F]) {
     def reify(s: Step): F[ReifiedStep] =   for {
             is <- s.inputs.traverse(store.load)
-        } yield ReifiedStep(is)
+            c <- store.load(s.cmd)
+            h <- store.hash(is ++ List(c))
+            o <- store.outputPathFromHash(h)
+        } yield ReifiedStep(is, c, o)
+}
+
+trait Runner[F[_]] {
+    def run(step: ReifiedStep): F[ExitCode]
 }
 
 object Flow {
